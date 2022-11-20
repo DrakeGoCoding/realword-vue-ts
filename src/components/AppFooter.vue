0423888ca024dc8ce74ddd7e3159f58c4c92aa1f
Phí Hoàng Long <57493556+DrakeGoCoding@users.noreply.github.com>
2022-11-20 15:30:41 +0700

    nah

--- src/components/AppFooter.vue
+++ src/components/AppFooter.vue
@@ -11,8 +11,8 @@
 		<div class="container">
 			<span class="attribution" style="margin: 0">
 				<a href="https://github.com/mutoe/vue3-realworld-example-app"
-					>Real world app</a
-				>
+					>Real world app
+				</a>
 				implementation using Vue v3 with Typescript and Composition Api
 			</span>
 		</div>
